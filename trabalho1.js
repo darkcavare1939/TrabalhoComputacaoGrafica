f37f40a88e311311032de3f437787a14872ce5f9
darkcavare1939 <67133337+darkcavare1939@users.noreply.github.com>
2021-01-24 22:01:52 -0300

    Add files via upload

--- trabalho1.js
+++ trabalho1.js
@@ -5,30 +5,15 @@ function main() {
     var camera = changeCamera(new THREE.Vector3(0, -25, 10)); // Init camera in this position
     var light = initDefaultLighting(scene, new THREE.Vector3(0, 10, 15));
     var trackballControls = new THREE.TrackballControls(camera, renderer.domElement);
-    var flag = 0;
-    var flag2 = 0;
-	var flag3 = 1;
-	var vetorChassi = new THREE.Vector3();
+    var vetorChassi = new THREE.Vector3();
 
     // Show text information onscreen
     showInformation();
 
     // To use the keyboard
     var keyboard = new KeyboardState();
-	//var keyboardEspaco = new KeyboardState();
 
     // Enable mouse rotation, pan, zoom etc.
-    //var trackballControls = new THREE.TrackballControls(camera, renderer.domElement);
-
-    // Show axes (parameter is size of each axis)
-    var axesHelper = new THREE.AxesHelper(12);
-    scene.add(axesHelper);
-
-    // create the ground plane
-    // add the plane to the scene
-    // plane = createPlane(100, 250);
-    // scene.add(plane);
-
     var planeGeometry = new THREE.PlaneGeometry(700, 700, 40, 40);
     planeGeometry.translate(0.0, 0.0, -0.02); // To avoid conflict with the axeshelper
     var planeMaterial = new THREE.MeshBasicMaterial({
@@ -49,23 +34,28 @@ function main() {
     // cabine e parte central do carro
     const chassi = cubo(2, 4.7, 0.5, 'rgb(255,255,255)');
     const cabine = cubo(1.5, 2, 0.2, 'rgb(92,88,88)');
-    var geometryTorus = new THREE.TorusGeometry(1.3, 0.35, 30, 3, 3);
+    var geometryTorus = new THREE.TorusGeometry(1.6, 0.25, 30, 3, 3);
     var materialTorus = new THREE.MeshBasicMaterial({ color: 'white' });
     const arcoEsquerdo = new THREE.Mesh(geometryTorus, materialTorus);
     const arcoDireito = new THREE.Mesh(geometryTorus, materialTorus);
-    const ponteCentral = cubo(1.4, 3.9, 0.4, 'blue');
+    const ponteCentral = cubo(1.5, 3.9, 0.4, 'blue');
     materialVolante = new THREE.MeshBasicMaterial({ color: 'black' });
     geometryVolante = new THREE.CylinderGeometry(0.3, 0.3, 0.1, 10);
     const volante = new THREE.Mesh(geometryVolante, materialVolante);
+    geometryVolante = new THREE.CylinderGeometry(0.1, 0.1, 1, 10);
+    const apoioVolante = new THREE.Mesh(geometryVolante, materialVolante);
+    const banco = cubo(0.1, 0.7, 0, 'rgb(0,255,0)');
+    const apoioBanco = cubo(0, 0.7, 0.1, 'rgb(0,255,0)');
 
     //pecas da frente do carro
-    const frenteDoCarro = cubo(1, 4.7, 0.5, 'rgb(255,0,0)');
-    const conexaoFrontal = cubo(0.8, 3.49, 1, 'rgb(0,0,255)');
+    const frenteDoCarro = cubo(1, 4.7, 0.5, 'rgb(255,255,255)');
+    const conexaoFrontal = cubo(0.8, 3.49, 0.7, 'rgb(0,0,255)');
     const asaEsquerdafrontal = cubo(0.7, 1, 0.1, 'rgb(0,255,0)');
     const asaDireitoFrontal = cubo(0.7, 1, 0.1, 'rgb(0,255,0)');
 
+
     // aerofolio do carro
-    const wing = cubo(2.7, 0.6, 0.2, 'rgb(0,0,255)');
+    const wing = cubo(4, 0.6, 0.2, 'rgb(0,0,255)');
     const leftWing = cubo(0.5, 0.8, 0.05, 'rgb(0,255,0)');
     const rightWing = cubo(0.5, 0.8, 0.05, 'rgb(0,255,0)');
     var geometrySuporte = new THREE.CylinderGeometry(0.1, 0.19, 1.6, 3, 5);
@@ -111,10 +101,11 @@ function main() {
 
     // Listen window size changes
     window.addEventListener('resize', function () { onWindowResize(camera, renderer) }, false);
-
-    buildInterface();
     render();
-
+	//variáveis que guardam qual foi última direção(frente ou trás) que o carro andou.
+    var flagDesaceleracaoAutomatica = 0;
+    var flagDesaceleracaoManual = 0;
+	
     // variavel que controla o angulo de rotacao das rodas para elas nao girarem 360*
     var anguloX = 0;
 
@@ -123,32 +114,20 @@ function main() {
     var inspect = false;
     //velocidade que o carro movimento no eixo Y
     var speed = 0.1;
+	//informa qual a rotação máxima das rodas  dianteiras.
     var anguloEsquerdoMaximo = -0.4067366430758069;
     var anguloDireitoMaximo = 0.30192189559966865;
-    //funcao principal que movimenta os carros
-    function keyboardUpdate() {
+    
+	//funcao principal que movimenta os carros
+    function movimentacaoCarro() {
         keyboard.update();
-        var position = new THREE.Vector3();
-        var quaternion = new THREE.Quaternion();
-        var scale = new THREE.Vector3();
-        /*var positionC = new THREE.Vector3();
-        var quaternionC = new THREE.Quaternion();
-        var scaleC = new THREE.Vector3();*/
-        camera.matrixAutoUpdate = true;
-        var rotX = Math.cos(anguloX);
-        var rotZ = Math.sin(anguloX);
-        var distance = 26.925824;
-        chassi.matrixWorld.decompose(position, quaternion, scale);
-        //camera.matrixWorld.decompose(positionC, quaternionC, scaleC);
 
-
-
+        //rotaciona as rodas
         if (keyboard.pressed("left") && anguloX > anguloEsquerdoMaximo) {
 
             rodaDianteiraDireita.matrix.multiply(mat4.makeRotationZ(degreesToRadians(anguloPadrao)));
             rodaDianteiraEsquerda.matrix.multiply(mat4.makeRotationZ(degreesToRadians(anguloPadrao)));
             anguloX = rodaDianteiraDireita.matrix.elements[0] + 0.1;
-            //plane.rotation.z = plane.rotation.z - 0.01;
         }
         if (keyboard.pressed("right") && anguloX < anguloDireitoMaximo) {
 
@@ -157,93 +136,83 @@ function main() {
             anguloX = rodaDianteiraDireita.matrix.elements[0] - 0.1;
 
         }
-        // o carro move para direcao do angulo em relacao a X
-        // 
-        if (keyboard.pressed("up")) {
-            flag = 1;
 
-            if (speed > 0 && flag2 == 2) {
-                chassi.matrix.multiply(mat4.makeTranslation(degreesToRadians(-anguloX), -speed, 0));
-                chassi.matrix.multiply(mat4.makeRotationZ(degreesToRadians(anguloX)));
-                speed = speed - 0.01;
-            }
+		//altera o modo da câmera  
+        if (keyboard.down("space")) changeCameraMode();
 
-            if (speed <= 0.1) {
-                flag2 = 1;
-            }
+		//impede a movimentação do carro no modo inspeção. 
+        if (inspect === false) {
+            var position = new THREE.Vector3();
+            var quaternion = new THREE.Quaternion();
+            var scale = new THREE.Vector3();
+            camera.matrixAutoUpdate = true;
+            var rotX = Math.cos(anguloX);
+            var rotZ = Math.sin(anguloX);
+            var distance = 26.925824;
+            chassi.matrixWorld.decompose(position, quaternion, scale);
 
-            if (flag2 == 1 && !keyboard.pressed("down")) {
-                chassi.matrix.multiply(mat4.makeTranslation(degreesToRadians(anguloX), speed, 0));
-                chassi.matrix.multiply(mat4.makeRotationZ(degreesToRadians(-anguloX)));
-                if (speed <= 3) {
-                    speed = speed + 0.01;
+            // o carro move para direcao do angulo em relacao a X
+            if (keyboard.pressed("up")) {
+                flagDesaceleracaoAutomatica = 1;
+                if (speed > 0 && flagDesaceleracaoManual == 2) {
+                    chassi.matrix.multiply(mat4.makeTranslation(degreesToRadians(-anguloX), -speed, 0));
+                    chassi.matrix.multiply(mat4.makeRotationZ(degreesToRadians(anguloX)));
+                    speed = speed - 0.01;
+                }
+
+                if (speed <= 0.1) {
+                    flagDesaceleracaoManual = 1;
+                }
+
+                if (flagDesaceleracaoManual == 1 && !keyboard.pressed("down")) {
+                    chassi.matrix.multiply(mat4.makeTranslation(degreesToRadians(anguloX), speed, 0));
+                    chassi.matrix.multiply(mat4.makeRotationZ(degreesToRadians(-anguloX)));
+                    if (speed <= 2) {
+                        speed = speed + 0.01;
+                    }
                 }
             }
-        }
-        if (keyboard.pressed("down")) {
-            flag = 2;
-            if (speed > 0 && flag2 == 1) {
-                chassi.matrix.multiply(mat4.makeTranslation(degreesToRadians(anguloX), speed, 0));
-                chassi.matrix.multiply(mat4.makeRotationZ(degreesToRadians(-anguloX)));
-                speed = speed - 0.01;
-            }
+            if (keyboard.pressed("down")) {
+                flagDesaceleracaoAutomatica = 2;
+                if (speed > 0 && flagDesaceleracaoManual == 1) {
+                    chassi.matrix.multiply(mat4.makeTranslation(degreesToRadians(anguloX), speed, 0));
+                    chassi.matrix.multiply(mat4.makeRotationZ(degreesToRadians(-anguloX)));
+                    speed = speed - 0.01;
+                }
 
-            if (speed <= 0.1) {
-                flag2 = 2;
-            }
+                if (speed <= 0.1) {
+                    flagDesaceleracaoManual = 2;
+                }
 
-            if (flag2 == 2 && !keyboard.pressed("up")) {
-                chassi.matrix.multiply(mat4.makeTranslation(degreesToRadians(-anguloX), -speed, 0));
-                chassi.matrix.multiply(mat4.makeRotationZ(degreesToRadians(anguloX)));
-                if (speed <= 3) {
-                    speed = speed + 0.01;
+                if (flagDesaceleracaoManual == 2 && !keyboard.pressed("up")) {
+                    chassi.matrix.multiply(mat4.makeTranslation(degreesToRadians(-anguloX), -speed, 0));
+                    chassi.matrix.multiply(mat4.makeRotationZ(degreesToRadians(anguloX)));
+                    if (speed <= 2) {
+                        speed = speed + 0.01;
+                    }
                 }
             }
-        }
-		
-        //Desacelera o carro quando não estiver apertando o acelerador
-        if (!keyboard.pressed("up") && !keyboard.pressed("down")) {
-            if (speed >= 0.1 && flag == 1) {
-                chassi.matrix.multiply(mat4.makeTranslation(degreesToRadians(anguloX), speed, 0));
-                chassi.matrix.multiply(mat4.makeRotationZ(degreesToRadians(-anguloX)));
-                speed = speed - 0.01;
-            }
 
-            if (speed >= 0.1 && flag == 2) {
-                chassi.matrix.multiply(mat4.makeTranslation(degreesToRadians(-anguloX), -speed, 0));
-                chassi.matrix.multiply(mat4.makeRotationZ(degreesToRadians(anguloX)));
-                speed = speed - 0.01;
-            }
+            //Desacelera o carro quando não estiver apertando o acelerador
+            if (!keyboard.pressed("up") && !keyboard.pressed("down")) {
+                if (speed >= 0.1 && flagDesaceleracaoAutomatica == 1) {
+                    chassi.matrix.multiply(mat4.makeTranslation(degreesToRadians(anguloX), speed, 0));
+                    chassi.matrix.multiply(mat4.makeRotationZ(degreesToRadians(-anguloX)));
+                    speed = speed - 0.01;
+                }
 
-        }
+                if (speed >= 0.1 && flagDesaceleracaoAutomatica == 2) {
+                    chassi.matrix.multiply(mat4.makeTranslation(degreesToRadians(-anguloX), -speed, 0));
+                    chassi.matrix.multiply(mat4.makeRotationZ(degreesToRadians(anguloX)));
+                    speed = speed - 0.01;
+                }
+
+            }
 			
-        camera.lookAt(position);
-		vetorCamera = position;
-
+            camera.lookAt(position);
+            vetorCamera = position;
+        }
     }
-	
-	/*function controleEspaco()
-	{
-		console.log("entrei");
-		//keyboardEspaco.update();
-		if (keyboardEspaco.down("W")) 
-		{
-			if(flag3 == 1)
-			{
-			  console.log("2");		
-			  flag3 = 2;	
-			  cameraModoInspecao();
-			  console.log(flag3);
-			  	
-			}
-			else
-			{
-			  console.log("1");	
-			  flag3 = 1;	
-			  cameraModoJogo();	              				  				
-			}
-		}
-	}*/
 
     function resetarCarro() {
         desligarAutoUpdate();
@@ -258,6 +227,8 @@ function main() {
         conexaoFrontal.matrixAutoUpdate = false;
         asaEsquerdafrontal.matrixAutoUpdate = false;
         asaDireitoFrontal.matrixAutoUpdate = false;
+        banco.matrixAutoUpdate = false;
+        apoioBanco.matrixAutoUpdate = false;
 
         //meio do carro
         chassi.matrixAutoUpdate = false;
@@ -266,6 +237,7 @@ function main() {
         arcoDireito.matrixAutoUpdate = false;
         cabine.matrixAutoUpdate = false;
         volante.matrixAutoUpdate = false;
+        apoioVolante.matrixAutoUpdate = false;
 
         //rodas e eixos
         rodaDianteiraDireita.matrixAutoUpdate = false;
@@ -297,6 +269,7 @@ function main() {
         ponteCentral.matrix.identity();
         cabine.matrix.identity();
         volante.matrix.identity();
+        apoioVolante.matrix.identity();
 
 
         //frente do carro
@@ -304,6 +277,8 @@ function main() {
         conexaoFrontal.matrix.identity();
         asaEsquerdafrontal.matrix.identity();
         asaDireitoFrontal.matrix.identity();
+        banco.matrix.identity();
+        apoioBanco.matrix.identity();
 
 
         //traseira
@@ -322,8 +297,6 @@ function main() {
         rodaTraseiraEsquerda.matrix.identity();
         eixoFrontal.matrix.identity();
         eixoTraseiro.matrix.identity();
-
-
     }
 
     // funcao que posiciona as partes inicais do carro
@@ -331,14 +304,17 @@ function main() {
         //frente do carro
         asaEsquerdafrontal.matrix.multiply(mat4.makeTranslation(-2.3, 4.5, 0.6));
         asaDireitoFrontal.matrix.multiply(mat4.makeTranslation(2.3, 4.5, 0.6));
+        banco.matrix.multiply(mat4.makeTranslation(0, 0, 0.3));
+        apoioBanco.matrix.multiply(mat4.makeTranslation(0, -0.3, 0.5));
         frenteDoCarro.matrix.multiply(mat4.makeTranslation(0, 4.5, 0));
-        conexaoFrontal.matrix.multiply(mat4.makeTranslation(0, 3.25, 0.5));
+        conexaoFrontal.matrix.multiply(mat4.makeTranslation(0, 3.25, 0.6));
 
 
         //meio do carro
         chassi.matrix.multiply(mat4.makeTranslation(0, 0, 1));
         cabine.matrix.multiply(mat4.makeTranslation(0, 0, 0.2));
-        volante.matrix.multiply(mat4.makeTranslation(0, 1.2, 0.7));
+        volante.matrix.multiply(mat4.makeTranslation(0, 1.2, 0.6));
+        apoioVolante.matrix.multiply(mat4.makeTranslation(0, 1.7, 0.6));
         ponteCentral.matrix.multiply(mat4.makeTranslation(0, 0.2, 0));
         arcoEsquerdo.matrix.multiply(mat4.makeTranslation(-0.6, 0.2, 0));
         arcoDireito.matrix.multiply(mat4.makeTranslation(0.6, 0.2, 0));
@@ -346,8 +322,8 @@ function main() {
 
         //traseira do carro
         wing.matrix.multiply(mat4.makeTranslation(0, -2, 1.5));
-        rightWing.matrix.multiply(mat4.makeTranslation(1.37, -2, 1.5));
-        leftWing.matrix.multiply(mat4.makeTranslation(-1.37, -2, 1.5));
+        rightWing.matrix.multiply(mat4.makeTranslation(2, -2, 1.5));
+        leftWing.matrix.multiply(mat4.makeTranslation(-2, -2, 1.5));
         escapamentoEsquerdo.matrix.multiply(mat4.makeTranslation(-0.4, -1.9, 0));
         escapamentoDireito.matrix.multiply(mat4.makeTranslation(0.4, -1.9, 0));
         suporteEsquerdoWing.matrix.multiply(mat4.makeTranslation(-0.5, -2, 0.76));
@@ -364,17 +340,19 @@ function main() {
 
     // funcao que cria os filhos da Chassi para o carro mover junto
     function createCarChild() {
-		
-		console.log('Entrei')
+
         //frente do carro
         chassi.add(frenteDoCarro);
         chassi.add(conexaoFrontal);
         chassi.add(asaEsquerdafrontal);
         chassi.add(asaDireitoFrontal);
+        chassi.add(banco);
+        chassi.add(apoioBanco);
 
         //meio do carro
         chassi.add(cabine);
         chassi.add(volante);
+        chassi.add(apoioVolante);
         chassi.add(arcoEsquerdo);
         chassi.add(arcoDireito);
         chassi.add(ponteCentral);
@@ -395,14 +373,19 @@ function main() {
         chassi.add(rodaTraseiraEsquerda);
         chassi.add(eixoFrontal);
         chassi.add(eixoTraseiro);
-        chassi.add(camera);
+        
+		//câmera 
+		chassi.add(camera);
     }
+
 
     // funcao que rotaciona o angulo das pecas para formar o carro
     function rotate() {
         //frente do carro
         asaEsquerdafrontal.matrix.multiply(mat4.makeRotationY(degreesToRadians(90)));
         asaDireitoFrontal.matrix.multiply(mat4.makeRotationY(degreesToRadians(90)));
+        banco.matrix.multiply(mat4.makeRotationY(degreesToRadians(90)));
+        apoioBanco.matrix.multiply(mat4.makeRotationX(degreesToRadians(90)));
         frenteDoCarro.matrix.multiply(mat4.makeRotationZ(degreesToRadians(90)));
 
         //meio do carro
@@ -426,7 +409,7 @@ function main() {
     }
 
 
-
+    // funcao que cria as rodas do carro
     function roda() {
         const cylinderGeometry = new THREE.CylinderGeometry(0.7, 0.7, 0.7, 17);
         const cylinderMaterial = new THREE.MeshPhongMaterial({ color: 'rgb(50,50,50)' });
@@ -450,10 +433,12 @@ function main() {
         return cubo;
     }
 
+	//altera a câmera para o modo inspeção 
     function cameraModoInspecao() {
-        
-      
         inspect = true;
+        chassi.remove(camera);
+        trackballControls = initTrackballControls(camera, renderer);
+        chassi.remove(cubeAxesHelper);
         camera.position.x = -15;
         camera.position.y = 15;
         camera.position.z = 15;
@@ -463,69 +448,54 @@ function main() {
         camera.up.z = 1;
         scene.remove(plane);
         scene.remove(line);
-
-        trackballControls = initTrackballControls(camera, renderer);
         resetarCarro();
     }
 
+	//altera a câmera para o modo jogo 
     function cameraModoJogo() {
+        
         inspect = false;
-        scene.add(plane);
-        scene.add(line);
-       camera = changeCamera(new THREE.Vector3(0, -25, 10));
-        resetarCarro();
-        /*var positionC = new THREE.Vector3();
-        var quaternionC = new THREE.Quaternion();
-        var scaleC = new THREE.Vector3();
-        camera.matrixWorld.decompose(positionC, quaternionC, scaleC);*/
-       
         speed = 0;
         anguloX = 0;
-        
-    }
+        chassi.add(cubeAxesHelper);
+        scene.add(plane);
+        scene.add(line);
+        camera = changeCamera(new THREE.Vector3(0, -25, 10));
+        resetarCarro();
+        chassi.add(camera);
+       }
 
+	// controla o modo da câmera 
+    function changeCameraMode() {
+        if (inspect === true) {
+            cameraModoJogo();
+        }
+        else {
+            cameraModoInspecao();
+        }
+    }
     function showInformation() {
         // Use this to show information onscreen
         controls = new InfoBox();
-        controls.add("Geometric Transformation");
         controls.addParagraph();
         controls.add("Use keyboard arrows to move the chassi.");
         controls.add("Press '< and '>' to rotate Wheels.");
+        controls.add("Press Space to change between modes");
         controls.show();
     }
 
-
-    function buildInterface() {
-        var controls = new function () {
-            this.cameraModoInspecao = function () {
-                cameraModoInspecao();
-				chassi.remove(camera);
-            };
-            this.cameraModoJogo = function () {
-					cameraModoJogo();
-					chassi.add(camera);
-            };
-        };
-
-        // GUI interface
-        var gui = new dat.GUI();
-        gui.add(controls, 'cameraModoInspecao').name("Modo de inspeção");
-        gui.add(controls, 'cameraModoJogo').name("Modo de Jogo");
-    }
 
     function render() {
         stats.update(); // Update FPS
         if (inspect === true)
             trackballControls.update();
-        if (inspect === false) {
-            keyboardUpdate();
-        }
-		//controleEspaco();
+        movimentacaoCarro();
         lightFollowingCamera(light, camera);
         requestAnimationFrame(render);
         renderer.render(scene, camera); // Render scene
     }
 
+    //configurações da câmera do jogo
     function changeCamera(initialPosition) {
         var position = initialPosition;
         var camera = new THREE.PerspectiveCamera(40, window.innerWidth / window.innerHeight, 0.1, 1000);
